3983c72d6fcf4af677b96a6210844e6cb0a7c9a5
Pedro Capelastegui <pedro@tuio.com>
2025-10-07 10:06:17 +0200

    Initial commit

---
+++ Model - LTV - Inference.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Test databricks environments for version controlled notebooks
+
+# COMMAND ----------
+
+from platform import python_version
+print(python_version())
+assert python_version() >= "3.12.3", "Wrong python version - please check databricks notebook environment version. >=v3 recommended."
+
+
+
